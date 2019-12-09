48c3a73362c812e193e7948407521f4791027684
AnnaMut <butorovaanna@yandex.ru>
2019-12-09 19:44:59 +0300

    mountain tabs

---
+++ source/js/script-mount.js
@@ -0,0 +1,20 @@
+var navMainMount = document.querySelector(".popup-menu-mountain");
+var navOpenMount = document.querySelector(".burger-menu-button-mountain");
+var navCloseMount = document.querySelector(".burger-menu-button-close-mountain");
+
+navOpenMount.addEventListener("click", function() {
+    if (navMainMount.classList.contains("closed-popup-menu-mountain")) {
+      navMainMount.classList.remove("closed-popup-menu-mountain");
+      navMainMount.classList.add("opened-popup-menu-mountain");
+    } /* else {
+      navMain.classList.add("closed-popup-menu");
+      navMain.classList.remove("opened-popup-menu");
+    } */
+  });
+  
+  navCloseMount.addEventListener("click", function() {
+      if (navMainMount.classList.contains("opened-popup-menu-mountain")) {
+        navMainMount.classList.remove("opened-popup-menu-mountain");
+        navMainMount.classList.add("closed-popup-menu-mountain");
+      } 
+    });
